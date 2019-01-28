77128254216a6ce52b42fd50b4203df92a8caa7d
your name here <theo@dali.dartmouth.edu>
2019-01-28 09:28:17 -0800

    normal quicksort

--- sorts.js
+++ sorts.js
@@ -1,13 +1,52 @@
 // quicksort in place
 
-function quicksortInPlace(arr) {
+// function quicksortInPlace(arr) {
+//   let pivot = Math.floor(Math.random() * arr.length); 
+//   if (arr.length <= 1) {
+//     return arr;
+//   }
+//   [arr[pivot], arr[0]] = [arr[0], arr[pivot]]
+//   pivot = 0;
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] < arr[0]) {
+//       [arr[i], arr[pivot + 1]] = [arr[pivot + 1], arr[i]]
+//       pivot += 1;
+//     }
+//   }
 
-};
+//   [arr[0], arr[pivot] = arr[pivot], arr[0]]
+//   quicksortInPlace(arr.slice(0, pivot));
+//   quicksortInPlace(arr.slice(pivot + 1));
+//   return arr;
+// };
 
-function mergeSort(arr) {
+// let test = [9, 2, 8, 3, 7, 4, 6, 1, 5];
 
-};
+// console.log(quicksortInPlace(test));
 
-function merge(left, right) {
+function quicksort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
 
-};
+  let pivot = Math.floor(Math.random() * arr.length); 
+  let left = arr.filter((el) => el < arr[pivot])
+  let right = arr.filter((el) => el > arr[pivot])
+
+  return quicksort(left).concat([arr[pivot]]).concat(quicksort(right))
+
+}
+
+let test = [9, 2, 8, 3, 7, 4, 6, 1, 5];
+
+console.log(quicksort(test));
+
+
+
+// function mergeSort(arr) {
+
+// };
+
+// function merge(left, right) {
+
+// };
