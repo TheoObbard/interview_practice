d0df90ed5ee112514a38d3cd5bd25cea6e1204e1
your name here <theo@dali.dartmouth.edu>
2019-01-29 17:11:33 -0800

    add max num in arr trick

--- tricks.js
+++ tricks.js
@@ -1,6 +1,6 @@
 // Tricks to simplify things JS doesn't make super easy
 
-// Get unique elements of arr - you could do this iteritively which is simple but 
+// 1) Get unique elements of arr - you could do this iteritively which is simple but 
 // in an interview you may just want a quick one liner so you can spend more time 
 // showing off your logic. Heres a super simple way that works in ES6: 
 
@@ -13,3 +13,19 @@ console.log(unique([1, 1, 2, 2, 2, 3, 3, 3, 3, 4, 4, 5, 5, 6]));
 // above is done in a func to test but it can be done anywhere by just setting a
 // variable to an array, spreading in a new Set and instantiating it with the original 
 // arr. 
+
+
+
+// 2) Get max from an array - again this can be done simply but is a pain to code out.
+// Here is a very simple implementation. 
+
+function max(arr) {
+  return arr.reduce((a, b) => {
+    return Math.max(a, b)
+  })
+}
+
+console.log(max([1, 2, 3, 4, 5, 90, 6]));
+
+// and again, it's done in a func here but you can just reduce, pass it a and b, and 
+// then return Math.max of a and b in a callback. 
