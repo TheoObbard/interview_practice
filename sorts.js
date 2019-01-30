1ed551f887bc84abdbd61892845357972ebc71d1
your name here <theo@dali.dartmouth.edu>
2019-01-29 17:02:32 -0800

    add bfs again

--- sorts.js
+++ sorts.js
@@ -1,72 +1,110 @@
-// quicksort in place
+// // quicksort in place
 
-function quicksortInPlace(arr) {
-  let pivot = Math.floor(Math.random() * arr.length); 
-  if (arr.length <= 1) {
-    return arr;
-  }
-  [arr[pivot], arr[0]] = [arr[0], arr[pivot]]
-  pivot = 0;
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] < arr[0]) {
-      [arr[i], arr[pivot + 1]] = [arr[pivot + 1], arr[i]]
-      pivot += 1;
-    }
-  }
-
-  [arr[0], arr[pivot] = arr[pivot], arr[0]]
-  quicksortInPlace(arr.slice(1, pivot));
-  quicksortInPlace(arr.slice(pivot + 1));
-  return arr;
-};
-
-let test = [9, 2, 8, 3, 7, 4, 6, 1, 5];
-
-console.log(quicksortInPlace(test));
-
-// function quicksort(arr) {
-//   if (arr.length < 2) {
+// function quicksortInPlace(arr) {
+//   let pivot = Math.floor(Math.random() * arr.length); 
+//   if (arr.length <= 1) {
 //     return arr;
 //   }
+//   [arr[pivot], arr[0]] = [arr[0], arr[pivot]]
+//   pivot = 0;
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] < arr[0]) {
+//       [arr[i], arr[pivot + 1]] = [arr[pivot + 1], arr[i]]
+//       pivot += 1;
+//     }
+//   }
 
-//   let pivot = Math.floor(Math.random() * arr.length); 
-//   let left = arr.filter((el) => el < arr[pivot])
-//   let right = arr.filter((el) => el > arr[pivot])
-
-//   return quicksort(left).concat([arr[pivot]]).concat(quicksort(right))
-
-// }
+//   [arr[0], arr[pivot] = arr[pivot], arr[0]]
+//   quicksortInPlace(arr.slice(1, pivot));
+//   quicksortInPlace(arr.slice(pivot + 1));
+//   return arr;
+// };
 
 // let test = [9, 2, 8, 3, 7, 4, 6, 1, 5];
 
-// console.log(quicksort(test));
+// console.log(quicksortInPlace(test));
+
+// // function quicksort(arr) {
+// //   if (arr.length < 2) {
+// //     return arr;
+// //   }
+
+// //   let pivot = Math.floor(Math.random() * arr.length); 
+// //   let left = arr.filter((el) => el < arr[pivot])
+// //   let right = arr.filter((el) => el > arr[pivot])
+
+// //   return quicksort(left).concat([arr[pivot]]).concat(quicksort(right))
+
+// // }
+
+// // let test = [9, 2, 8, 3, 7, 4, 6, 1, 5];
+
+// // console.log(quicksort(test));
 
 
 
-// function mergeSort(arr) {
+// // function mergeSort(arr) {
 
+// // };
+
+// // function merge(left, right) {
+
+// // };
+
+// function post(node) {
+//   if (node.null) {
+//     return [];
+//   }
+
+//   let left = post(node.left);
+//   let right = post(node.right);
+
+//   return left.concat(right).push(node);
 // };
 
-// function merge(left, right) {
+// function dfs(node, target) {
+//   if (node.value === target) {
+//     return node;
+//   }
 
+//   dfs(node.left);
+//   dfs(node.right);
+// }
+
+// var longestPalindrome = function (s) {
+//   let longest = 0;
+//   let ans = null;
+//   let arr = s.split('');  
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//       let section = arr.splice(i, j);
+//       let rev = section.reverse();
+//       if (section === rev) {
+//         if (section.length > longest) {
+//           longest = section.length;
+//           ans = section.join('')
+//         }
+//       }
+//     }
+//   }
+//   return ans;
 // };
 
-function post(node) {
-  if (node.null) {
-    return [];
+// console.log(longestPalindrome('abacdabba'));
+
+function bfs(node, target) {
+  let queue = [node];
+  while (queue.length > 0) {
+    let current = queue.shift();
+    if (current.value === target) {
+      return current;
+    }
+    current.visited = true;
+    current.outEdges.forEach(node => {
+      if (!node.visited) {
+        queue.push(node);
+      }
+    })
   }
-
-  let left = post(node.left);
-  let right = post(node.right);
-
-  return left.concat(right).push(node);
-};
-
-function dfs(node, target) {
-  if (node.value === target) {
-    return node;
-  }
-
-  dfs(node.left);
-  dfs(node.right);
+  return null;
 }
