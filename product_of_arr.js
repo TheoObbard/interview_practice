ebf3b3fa984bca85fa1b3dcd8eaae5fcb8ad43b1
your name here <theo@dali.dartmouth.edu>
2019-02-12 11:14:01 -0800

    setup problem

---
+++ product_of_arr.js
@@ -0,0 +1,10 @@
+// given an arr of ints, return an arr where i is the product of arr except for i
+// input = [2, 3, 4, 5]
+// output = [60, 40, 30, 24]
+
+// note: division is not allowed
+// note: do this in linear time
+
+function productFind(arr) {
+
+}
