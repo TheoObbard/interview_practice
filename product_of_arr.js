0f749b89c5d0ac902f31160b16c956fc77ca918c
your name here <theo@dali.dartmouth.edu>
2019-02-12 11:40:04 -0800

    working solution to productFind

--- product_of_arr.js
+++ product_of_arr.js
@@ -6,5 +6,29 @@
 // note: do this in linear time
 
 function productFind(arr) {
+ 
+  // iterate over once to product all elements to the left and insert them
+
+  let ans = [];
+  let leftProduct = 1;
+  for (let i = 0; i < arr.length; i++) {
+    ans.push(leftProduct);
+    leftProduct = arr[i] * leftProduct;
+  }
+  let rightProduct = 1;
+  for (let i = arr.length - 1; i >= 0; i--) {
+    ans[i] = ans[i] * rightProduct;
+    rightProduct *= arr[i]
+  }
+
+  return ans;
+  // iterate over again to product all els to right and product them
+
+  // inp = [1, 2, 3, 4]
+  // ans = [];
+
+  // out = [24, 12, 8, 6]
 
 }
+
+console.log(productFind([1, 2, 3, 4]));
