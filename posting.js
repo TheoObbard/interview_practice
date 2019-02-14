e50a18aaf9f06ca54cf289cb0e9375eed3190814
your name here <theo@dali.dartmouth.edu>
2019-02-14 11:28:22 -0800

    dom manipulation practice w quick posting site

---
+++ posting.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', () => {
+  createElements();
+});
+
+function createPost() {
+  let inp = document.querySelectorAll('INPUT')[0];
+  let content = inp.value;
+  inp.value = '';
+  let item = document.createElement('LI');
+  item.id = 'list_item';
+  item.innerHTML = content;
+  let form = commentForm();
+  item.appendChild(form);
+  let list = document.querySelector('UL');
+  list.appendChild(item);
+}
+
+function createElements() {
+  let body = document.getElementsByTagName('BODY')[0];
+  let inp = document.createElement('INPUT');
+  body.appendChild(inp);
+  let btn = document.createElement('BUTTON');
+  btn.innerHTML = 'submit new post';
+  btn.onclick = createPost;
+  body.appendChild(btn);
+  let list = document.createElement('UL');
+  list.id = 'all_posts';
+  body.appendChild(list);
+}
+
+function commentForm() {
+  let inp = document.createElement('INPUT');
+  let btn = document.createElement('BUTTON');
+  btn.innerHTML = 'leave a comment'
+  btn.onclick = addComment;
+  let list = document.createElement('UL');
+  list.id = 'comments'
+  let dv = document.createElement('DIV');
+  dv.appendChild(inp);
+  dv.appendChild(btn);
+  dv.appendChild(list);
+
+  return dv;
+}
+
+function addComment() {
+  let list = this.parent.children[2];
+  console.log(list)
+  let item = document.createElement('LI');
+  item.innerHTML = 'test'
+  list.appendChild(item);
+}
