9e292023075e2b380de1c5fa0c3f925d200635c2
your name here <theo@dali.dartmouth.edu>
2019-02-25 10:54:19 -0800

    fast way to check for duplicates in an array and return them

--- warmup.js
+++ warmup.js
@@ -15,3 +15,17 @@ function quicksort(arr) {
 let test = [9, 4, 8, 2, 5, 6, 7, 1, 0];
 
 // console.log(quicksort(test))
+
+// find duplicates in an arr
+
+var findDuplicates = function (nums) {
+  let result = [];
+  nums = nums.sort((a, b) => a - b);
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === nums[i + 1]) {
+      result.push(nums[i + 1]);
+    }
+  }
+  result = [...new Set(result)];
+  return result;
+};
