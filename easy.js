1de25e0f21e06e943a32b70ca4a766de629ee068
Theo Obbard <theo@dali.dartmouth.edu>
2019-03-07 11:30:33 -0800

    add reverse number solution

--- easy.js
+++ easy.js
@@ -1,1 +1,24 @@
 
+// reverse an integer, can be negative
+
+var reverse = function(x) {
+    let str = x + "";
+    let negative = false;
+    if (str[0] === '-') {
+        negative = true;
+        str = str.split('');
+        str.shift();
+        str = str.join('')
+    }
+    let arr = str.split('');
+    let reved = arr.reverse()
+
+    reved = reved.join('');
+    let newNum = parseInt(reved);
+    if (negative) {
+        return newNum * -1;
+    }
+    return newNum;
+};
+
+console.log(reverse(-321));
